366167180b0da71db0aff8ce37cdd7efafd86ecb
Gregor Gresak <design@gresak.net>
2015-05-10 16:03:05 +0200

    zaprt komentar

--- zanke.php
+++ zanke.php
@@ -37,3 +37,4 @@ foreach($seznam as $key => $item) {
 2. Uporabi smiseln break v vsaki izmed zank, lahko narediš tudi zanko po svoje
 3. napiši funkcijo vrni_podatek
 4. ZA PIFLARJE: naredi to kar dela while zanka s for zanko
+**/
