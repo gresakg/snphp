78f489200f7418c1692f7ecced1753d9a70d0672
Gregor Gresak <design@gresak.net>
2015-05-10 16:01:39 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php 
+// preveri ali je obrazec oddan
+if(isset($_POST['stevilo'])) {
+	// obrazec je oddan
+	$stevilo = $_POST['stevilo'];
+	$skrito = $_POST['skrito'];
+	$stevec = $_POST['stevec'] + 1;
+	
+	if(!is_numeric($stevilo)) {
+		?>
+		To ni število!
+		<?php
+	} else {
+		
+		 preveri_zadetek($skrito, $stevilo);
+		?>
+<?php
+	}
+} else {
+	//obrazec ni oddan
+	$stevilo = "";
+	$skrito = rand(1,5);
+	$stevec = 0;
+}
+?>
+<form method="post">
+Skrito število:
+<input type="text" name="stevilo" id="stevilo" 
+	   value="<?php echo $stevilo; ?>"> <?php echo $skrito; ?>
+	
+<input type="hidden" name="skrito" value="<?php echo $skrito; ?>">
+<input type="hidden" name="stevec" value="<?php echo $stevec; ?>">
+<input type="submit" value="Oddaj">
+
+</form>
+Poskusili ste <?php echo $stevec ?> krat.
+
+
+<?php
+	
+function preveri_zadetek($skrito, $stevilo) {
+	
+	if($stevilo == $skrito) {
+		return "Bravo!";
+		
+	} elseif ($stevilo > $skrito) {
+		return "Vnesli ste preveliko število!";	
+		
+	} elseif( $stevilo < $skrito) {
+		return "Vnesli ste premajhno število!";
+	}
+	
+}
+
+
+
+
---
+++ zanke.php
@@ -0,0 +1,39 @@
+<?php 
+
+for($i=1;$i<6;$i=$i+1) {
+	echo $i;
+	echo "<br>";
+}
+
+$skrito = rand(1,5);
+$i=0;
+while($i != $skrito) {
+	echo $i;
+	echo " ni pravo število<br>";
+	$i++;
+}
+echo $i . " je pravo število<br>";
+
+$seznam = array(
+	"ime" => "Janez", 
+	"priimek"=>"Novak", 
+	"leto"=>1966
+);
+$seznam["spol"] = "moški";
+
+
+echo "<h1>".$seznam["ime"]."</h1>";
+
+foreach($seznam as $key => $item) {
+	//echo vrni_podatek(---);
+	
+	echo $key.": ".$item;
+	echo "<br>";
+	
+}
+
+/**
+1. preglej "skrito število" in izboljšaj ali spremeni
+2. Uporabi smiseln break v vsaki izmed zank, lahko narediš tudi zanko po svoje
+3. napiši funkcijo vrni_podatek
+4. ZA PIFLARJE: naredi to kar dela while zanka s for zanko
