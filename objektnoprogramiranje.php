0193785e2a5abf5334de9051419adb8fb7ce682a
Gregor Gresak <design@gresak.net>
2015-05-15 18:25:12 +0200

    public

--- objektnoprogramiranje.php
+++ objektnoprogramiranje.php
@@ -1,9 +1,5 @@
 <?php
-//Naloga: Ustvarite razred Restaurant, ki ima lastnosti $name za ime restavracije, $city za kraj, v katerem se nahaja restavracija in $tables za število miz na voljo v restavraciji.
 
-//Ustvarite metodo razreda Restaurant location(), ki vrne informacijo o imenu restavracije in kraju, v katerem se restavracija nahaja.
-
-//Ustvarite metodo razreda Restaurant welcome(), ki gosta pozdravi in mu sporoči število prostih miz. Poskrbite, da se bo število prostih miz zmanjšalo za 1 vsakič, ko se pokliče metoda welcome()
 
 
 
@@ -44,8 +40,8 @@ class Restaurant {
     public $city;
     public $tables;
 	public $kontakt;
-	private $natakarji = array("peter");
-	private $stNatakarjev;
+	public $natakarji = array("peter");
+	public $stNatakarjev;
 
     public function __construct($name, $city, $tables) {
         $this->name = $name;
