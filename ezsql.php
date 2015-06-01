b58f7b168cd19c08614470be83a8c140d9657a05
Gregor Gresak <design@gresak.net>
2015-06-01 08:19:10 +0200

    dodan uforeport

--- ezsql.php
+++ ezsql.php
@@ -3,7 +3,7 @@
 require "ezSQL/shared/ez_sql_core.php";
 require "ezSQL/mysql/ez_sql_mysql.php";
 
-$db = new ezSQL_mysql('aplikacija','12345','world','localhost');
+$db = new ezSQL_mysql('root','','world','localhost');
 
 $db->query('DELETE from City WHERE Name="Ptuj"');
 
